7b27a51d051286195f48b29c85fb794d98720ac4
AndreyKapus <kapustnikov@ukr.net>
2022-10-03 22:22:55 +0300

    searchbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ToastContainer } from 'react-toastify';
+
+const axios = require('axios');
+
+// const api_key = '29432159-064ba5645d6ae7f18ff2bb6d2';
+
+export class App extends React.Component {
+  state = {
+    picture: '',
+  };
+
+  handleFormSubmit = picture => {
+    this.setState({ picture });
+    console.log(picture);
+  };
+
+  // componentDidMount() {
+  //   axios
+  //     .get(
+  //       'https://pixabay.com/api/?q=cat&page=1&key=29432159-064ba5645d6ae7f18ff2bb6d2&image_type=photo&orientation=horizontal&per_page=12'
+  //     )
+  //     .then(picture => this.setState({ picture }));
+  // }
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import css from './Searchbar.module.css';
+import { FaSearchLocation } from 'react-icons/fa';
+
+export class Searchbar extends React.Component {
+  state = {
+    picture: '',
+  };
+
+  handlePicNameChange = event => {
+    this.setState({ picture: event.currentTarget.value.toLowerCase() });
+    console.log(this.state);
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    if (this.state.picture.trim() === '') {
+      alert('Введите название картинки');
+      return;
+    }
+    this.props.onSubmit(this.state.picture);
+
+    this.setState({ picture: '' });
+  };
+
+  render() {
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+          <button type="submit" className={css.SearchFormButton}>
+            <FaSearchLocation />
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handlePicNameChange}
+          />
+        </form>
+      </header>
+    );
+  }
+}
