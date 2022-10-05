2abdbb36d3c75a811144c27c6e26d1a8f5368033
AndreyKapus <kapustnikov@ukr.net>
2022-10-05 16:28:33 +0300

    fetchImages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,16 +14,7 @@ export class App extends React.Component {
 
   handleFormSubmit = picture => {
     this.setState({ picture });
-    console.log(picture);
   };
-
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       'https://pixabay.com/api/?q=cat&page=1&key=29432159-064ba5645d6ae7f18ff2bb6d2&image_type=photo&orientation=horizontal&per_page=12'
-  //     )
-  //     .then(picture => this.setState({ picture }));
-  // }
 
   render() {
     return (
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -2,6 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 import { App } from 'components/App';
+import css from './ImageGallry.module.css';
 
 // key = 29432159-064ba5645d6ae7f18ff2bb6d2
 
@@ -12,32 +13,29 @@ export class ImageGallery extends React.Component {
 
   componentDidUpdate(prevProps, prevState) {
     if (prevProps.pictureName !== this.props.pictureName) {
-      console.log('prev.props', prevProps.pictureName);
-      console.log('this.props', this.props.pictureName);
-
       axios
         .get(
           `https://pixabay.com/api/?q=${this.props.pictureName}&page=1&key=29432159-064ba5645d6ae7f18ff2bb6d2&image_type=photo&orientation=horizontal&per_page=12`
         )
         .then(res => this.setState({ picture: res.data.hits }))
         .catch(console.log('error'));
-      console.log(this.state.picture);
-
-      console.log('Изменилась картинка');
     }
   }
 
   render() {
-    const { picture } = this.props;
+    const { picture } = this.state;
     console.log(this.state.picture);
     return (
       <div>
-        <ul className="gallery">
+        <ul className={css.ImageGallery}>
           {picture &&
-            picture.map(img => (
-              <li title={img.tags} key={img.id}>
-                <img src={img.largeImageURL} alt="somepicture" width={300} />
-                <span>какой-то текст</span>
+            picture.map(({ id, webformatURL, tags, largeImageURL }) => (
+              <li title={tags} key={id} className={css.ImageGalleryItem}>
+                <img
+                  src={largeImageURL}
+                  alt="somepicture"
+                  className={css.img}
+                />
               </li>
             ))}
         </ul>
